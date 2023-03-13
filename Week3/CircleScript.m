edf221482c45ec7d29234edc8eba979e3fc60caf
ThaveeshaWBasnayake <127285138+ThaveeshaWBasnayake@users.noreply.github.com>
2023-03-13 22:54:27 +0900

    Add files via upload

---
+++ Week3/CircleScript.m
@@ -0,0 +1,6 @@
+%This script computes the area and circumference of a circle
+radius = 4;
+area = pi*radius^2;
+disp('area ='); disp(area);
+circum = 2*pi*radius;
+disp('circumference ='); disp(circum);

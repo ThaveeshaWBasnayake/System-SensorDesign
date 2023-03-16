229bda9e3269fdd63af403b4f76effaff9f27c55
ThaveeshaWBasnayake <127285138+ThaveeshaWBasnayake@users.noreply.github.com>
2023-03-16 23:46:30 +0900

    Add files via upload

---
+++ Example2.m
@@ -0,0 +1,8 @@
+a = 4;
+b = 17.2;
+FirstProb = 5*a;
+SecondProb = nthroot(b+9.8,3);
+ThirdProb = 10*sqrt(5*a+16);
+disp('FirstProb = '); disp(FirstProb);
+disp('SecondProb = '); disp(SecondProb);
+disp('ThirdProb = '); disp(ThirdProb);

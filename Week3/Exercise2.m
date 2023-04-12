bafc327e53fbee21b455f4dcc0d06d2b03da9526
ThaveeshaWBasnayake <127285138+ThaveeshaWBasnayake@users.noreply.github.com>
2023-04-12 15:20:58 +0900

    Update Exercise2.m

--- Week3/Exercise2.m
+++ Week3/Exercise2.m
@@ -1,3 +1,8 @@
-t = 0:0.02:0.6;
-y = 3*sin(2*pi*10*t);
-plot(t,y);
+t = 0:0.02:0.6; %Creates a time domain for the function to lie in
+y = 3*sin(2*pi*10*t); %Defines the sinusoidal function
+plot(t,y); %plots the graph t vs y
+
+%Names the axis and gives title
+xlabel('t');
+ylabel('y(t)');
+title('Sinusoidal Wave');
